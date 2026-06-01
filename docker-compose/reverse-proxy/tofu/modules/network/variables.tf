0de2c52ac5be688150a2c4b1ec6160471537506e
Charles LESECQ <charles@lesecq.eu>
2026-06-01 12:53:23 +0200

    chore: remove merged files

--- docker-compose/reverse-proxy/tofu/modules/network/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "peers" {
-  type        = list(string)
-  description = "List of peer IP addresses for the VXLAN zone"
-}
--- docker-compose/reverse-proxy/tofu/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "flatcar_nodes" {
-  description = "Map of all Flatcar Linux node details (VM IDs, names, IP addresses)"
-  value       = module.flatcar_nodes.vms
-}
